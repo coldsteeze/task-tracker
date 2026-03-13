2eaa4201b92738e09429c17e48d33d8587a14127
coldsteeze <nikitaproduction@mail.ru>
2026-03-13 15:11:04 +0300

    fix(auth): correct request matchers in SecurityConfig

--- auth-service/src/main/java/korobkin/nikita/auth_service/security/SecurityConfig.java
+++ auth-service/src/main/java/korobkin/nikita/auth_service/security/SecurityConfig.java
@@ -26,8 +26,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/users/**").authenticated()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/users/**").authenticated()
                         .anyRequest().denyAll()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
