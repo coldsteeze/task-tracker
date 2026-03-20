04ca3653599d3bd1e4d29d630710603fa1657e5a
coldsteeze <nikitaproduction@mail.ru>
2026-03-20 11:17:52 +0300

    feat(notification): add NotificationMapper, NotificationRepository, NotificationService

---
+++ notification-service/src/main/java/korobkin/nikita/notification_service/service/NotificationService.java
@@ -0,0 +1,21 @@
+package korobkin.nikita.notification_service.service;
+
+import korobkin.nikita.notification_service.dto.NotificationFilterRequest;
+import korobkin.nikita.notification_service.dto.NotificationResponse;
+import korobkin.nikita.notification_service.dto.PagedResponse;
+import korobkin.nikita.task_events.TaskCreatedEvent;
+import korobkin.nikita.task_events.TaskStatusChangedEvent;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface NotificationService {
+
+    void createTaskCreatedNotification(TaskCreatedEvent event);
+
+    void createStatusChangedNotification(TaskStatusChangedEvent event);
+
+    PagedResponse<NotificationResponse> getNotifications(NotificationFilterRequest request, Pageable pageable);
+
+    NotificationResponse getNotification(UUID id);
+}
